cb1c25299ac200a1c149d8b5b5e56d1f466143c9
raymondproduction@ukr.net <raymondproduction@ukr.net>
2017-02-10 02:48:27 +0200

    Update

--- server.js
+++ server.js
@@ -1,42 +1,104 @@
-var koa = require('koa');
-var app = koa();
-var fs = require('fs');
-var path = require('path');
+// модуль для цвета
+var colors = require('colors');
+// модуль для роботы с ip адресами
+var ip = require('ip');
+// web framework for node.js
+var app = require('koa')();
+// определим номер порта, чере ключ в командной строке
 var port = parametr('--port');
-var buf;
-var str;
-var rootDir= parametr('--directory');
-var staticContent = {'.html' : 'text/html; charset=utf-8',
-                     '.css'  : 'text/css; charset=utf-8',
-                     '.js'   : 'text/plain',
-                     '.jpg'  : 'image/jpg',
-                     '.png'  : 'image/png'
-                    };
 
-console.log('Start Web Server');
+//  если порта нет то используем по умолчянию 8080
+if (port) {
+  server = app.listen(Number(port));
+} else {
+  server = app.listen(8080);
+}
 
-app.use(function *(next){
+// это важно! подключаем сокет к серверу
+// сокет - розетка, модуль для мгновенной
+// передачи данных
+var io = require('socket.io').listen(server);
+
+// модуль для роботы с файловой системной
+// так как сервер будет использовать файлы
+var fs = require('fs');
+// используем модуль path для роботы
+// с путями к файлам
+var path = require('path');
+
+var buf; // буфер байт из файла
+var str; // строка из файла
+var rootDir = parametr('--directory'); // определим корневую папку
+// контекстный массив для определения основных
+// типов файлов
+var staticContent = {
+  '.html': 'text/html; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.js': 'text/plain',
+  '.jpg': 'image/jpg',
+  '.png': 'image/png'
+};
+
+// Вывод в консоль уведомления о запуске сервера и ip веб сервера
+console.log('Start Web Server'.green.bold,' Local ip address is '.cyan,ip.address().cyan);
+
+app.use(function*(next) {
   yield next;
   var url = this.url;
-  console.log('GET => ',url,'\tContent type: ',staticContent[path.extname(url)]);
-  if (url=='/') {url='/index.html';}
+  console.log('GET => '.magenta, url, '\tContent type: '.yellow, staticContent[path.extname(url)]);
+  if (url == '/') {
+    url = '/index.html';
+  }
   this.type = staticContent[path.extname(url)];
-  buf = fs.readFileSync(rootDir+url);
- // str = buf.toString();
-  str = buf;
+  if (fs.existsSync(rootDir + url)) {
+    buf = fs.readFileSync(rootDir + url);
+    // str = buf.toString();
+    str = buf;
+  } else {
+    console.log('Not found'.red);
+    str='Not found :(';
+  }
+
   this.body = str;
 });
 
-if (port) {app.listen(Number(port));}
-else      {app.listen(8080);}
 
 
-function parametr(par){
+io.on('join', function*() {
+  console.log('join event fired', this.data)
+})
+
+// оброботка события подключения, или отключения от сокета
+io.on('connection', function(socket) {
+  console.log('Socket=>'.blue, ' a user connected');
+  socket.on('disconnect', function() {
+    console.log('Socket=>'.blue, ' user disconnected');
+  });
+
+});
+
+// вывод входящих сообщений на консоль
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    console.log('Socket=>'.blue, ' message: ' + msg);
+  });
+});
+
+// отправка методом emit входящих сообщений назад
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
+});
+
+
+
+function parametr(par) {
   var res;
   process.argv.forEach(function(item, i, arr) {
-     if (par==item) {
-           res =  process.argv[i+1];
-     }
-  }); 
+    if (par == item) {
+      res = process.argv[i + 1];
+    }
+  });
   return res;
 }
